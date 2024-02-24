7cf224e7df654d222ccc08c44b3bc3722317e68d
ryan <concepting@protonmail.com>
2024-02-23 22:16:59 -0500

    Added WaiterGUI.java Class

---
+++ src/main/java/ui/components/WaiterGUI.java
@@ -0,0 +1,18 @@
+package ui.components;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class WaiterGUI {
+    private JFrame welcomeUser;
+    private JPanel panel;
+
+    public WaiterGUI(Font welcomeFont){
+        panel = new JPanel(new GridBagLayout());
+
+
+
+    }
+
+}
