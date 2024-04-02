f3298353bb0d365e89dee35e1d84be495f0f3cfe
Shams Hasan <105460072+shamshasan0@users.noreply.github.com>
2024-04-02 01:02:32 -0400

    Update sql file to remove names

--- scripts/sql/employeeTables.sql
+++ scripts/sql/employeeTables.sql
@@ -14,7 +14,6 @@ DROP TABLE IF EXISTS jrestaurant_managers;
 create table jrestaurant_managers(
 
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name varchar(50) NOT NULL,
     role varchar(50),
     manager_username varchar(50),
     manager_password varchar(50)
@@ -28,7 +27,6 @@ create table jrestaurant_waiters(
 -- 'auto increment' means that it will increase by +1 
 
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name varchar(100) NOT NULL,
     role varchar(50),
     waiter_username varchar(50),
     waiter_password varchar(50)
@@ -41,7 +39,6 @@ create table jrestaurant_waiters(
 create table jrestaurant_busboys(
 
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name varchar(100) NOT NULL,
     role varchar(50),
     busboy_username varchar(50),
     busboy_password varchar(50)
@@ -52,7 +49,6 @@ create table jrestaurant_busboys(
 create table jrestaurant_cooks(
 
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name varchar(100) NOT NULL,
     role varchar(50),
     cook_username varchar(50),
     cook_password varchar(50)
@@ -64,7 +60,6 @@ create table jrestaurant_cooks(
 create table jrestaurant_hosts(
 
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name varchar(100) NOT NULL,
     role varchar(50),
     host_username varchar(50),
     host_password varchar(50)
@@ -75,36 +70,36 @@ create table jrestaurant_hosts(
 -- here we insert values for each employee
 
 -- managers
-insert into jrestaurant_managers(name, role, manager_username, manager_password)
-values ('Jeff', 'manager', 'm1user', 'm1password');
+insert into jrestaurant_managers(role, manager_username, manager_password)
+values ('manager', 'm1user', 'm1password');
 
 
 
 -- waiters
-insert into jrestaurant_waiters(name, role, waiter_username, waiter_password)
-values ('Sarah', 'waiter', 'w1user', 'w1password'),
-       ('Josh', 'waiter', 'w2user', 'w2password'),
-       ('Mike', 'waiter', 'w3user', 'w3password');
+insert into jrestaurant_waiters(role, waiter_username, waiter_password)
+values ('waiter', 'w1user', 'w1password'),
+       ('waiter', 'w2user', 'w2password'),
+       ('waiter', 'w3user', 'w3password');
 
 
 
 -- busboys
-insert into jrestaurant_busboys(name, role, busboy_username, busboy_password)
-values ('Cameron', 'busboy', 'b1user', 'b1password');
+insert into jrestaurant_busboys(role, busboy_username, busboy_password)
+values ('busboy', 'b1user', 'b1password');
 
 
 
 
 -- cooks
-insert into jrestaurant_cooks (name, role, cook_username, cook_password)
-values ('Lily', 'cook', 'cuser1', 'c1password'),
-    ('Charlotte', 'cook', 'c2user', 'c2password');
+insert into jrestaurant_cooks (role, cook_username, cook_password)
+values ('cook', 'cuser1', 'c1password'),
+    ('cook', 'c2user', 'c2password');
 
 
 
 -- hosts
-insert into jrestaurant_hosts(name, role, host_username, host_password)
-values ('Dean', 'host', 'huser1', 'h1password');
+insert into jrestaurant_hosts(role, host_username, host_password)
+values ('host', 'huser1', 'h1password');
 
 
 
