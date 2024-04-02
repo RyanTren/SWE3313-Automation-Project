e7a7de7ef11dab302be5c19c5baf241a669481da
ryan <concepting@protonmail.com>
2024-04-01 23:55:30 -0400

    sum light lil clean up

--- src/main/java/org/jsrestaurant/App.java
+++ src/main/java/org/jsrestaurant/App.java
@@ -10,32 +10,27 @@ public class App {
         // main application window
         Font appFont = new Font("Franklin Gothic Medium", Font.ITALIC, 18);
         JFrame loginFrame = new JFrame("J's Restaurant");
-//        JFrame waiterFrame = new JFrame("Waiter");
 
         //Full Screens the Application
-       Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-       loginFrame.setPreferredSize(screenSize);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        loginFrame.setPreferredSize(screenSize);
 
-       //frame.setPreferredSize();
-       loginFrame.setFont(appFont);
-       loginFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        loginFrame.setFont(appFont);
+        loginFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        //this line doesn't work for some reason but for the other java files it does...
+//      loginFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
         // Add application components
-       Font loginFont = new Font("Franklin Gothic Medium", Font.PLAIN, 18); // Customize the font here
-       loginFrame.setContentPane(new LoginGUI(loginFont).getPanel());
+        Font loginFont = new Font("Franklin Gothic Medium", Font.PLAIN, 18); // Customize the font here
+        loginFrame.setContentPane(new LoginGUI(loginFont).getPanel());
 
-       loginFrame.pack();
-       loginFrame.setLocationRelativeTo(null);
-       loginFrame.setVisible(true);
-       Font waiterFont = new Font("Franklin Gothic Medium", Font.PLAIN, 18); // Customize the font here
-       loginFrame.setContentPane(new LoginGUI(waiterFont).getPanel());
+        loginFrame.pack();
+        loginFrame.setLocationRelativeTo(null);
+        loginFrame.setVisible(true);
+        Font waiterFont = new Font("Franklin Gothic Medium", Font.PLAIN, 18); // Customize the font here
+        loginFrame.setContentPane(new LoginGUI(waiterFont).getPanel());
 
-
-       loginFrame.pack();
-
-       //waiterFrame isn't finished atm
-//       waiterFrame.pack();
-//       waiterFrame.setVisible(true);
-
+        loginFrame.pack();
    }
 }
