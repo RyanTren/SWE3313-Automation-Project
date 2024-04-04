b679c11ccb3356551cb6d769ca067677bccf22d9
ryan <concepting@protonmail.com>
2024-04-04 12:37:57 -0400

    Added small guide for Nhut or Isa in App.java
    instructions on how to run program.

--- src/main/java/org/jsrestaurant/App.java
+++ src/main/java/org/jsrestaurant/App.java
@@ -5,6 +5,20 @@ import ui.components.LoginGUI;
 import javax.swing.*;
 import java.awt.*;
 
+
+
+/*
+For any members who are testing the application, you must first run Docker...
+
+Go to Terminal, fetch this repo, type "docker compose up -d" and wait for it to start.
+Once this is done, run App.java.
+
+If the log-in for any of the Employees don't work: terminate the program, go back to the terminal
+and type "docker compose down --volumes" and type "docker compose up -d" again.
+
+All of the GUI's will run off of App.java
+ */
+
 public class App {
    public static void main(String[] args) {
         // main application window
