c007ccec6a6f1ba6d91fb1a0b7ca3639154371f6
Shams Hasan <105460072+shamshasan0@users.noreply.github.com>
2024-04-07 19:37:47 -0400

    formatted document

--- scripts/sql/employeeTables.sql
+++ scripts/sql/employeeTables.sql
@@ -5,24 +5,58 @@ DROP TABLE IF EXISTS restaurant_statistics;
 
 create table jrestaurant_employees
 (
-    id          INT AUTO_INCREMENT PRIMARY KEY,
-    name        varchar(100) NOT NULL,
-    role        varchar(100) NOT NULL,
-    username    varchar(100) NOT NULL,
-    password    varchar(100) NOT NULL,
+    id INT
+    AUTO_INCREMENT PRIMARY KEY,
+    name        varchar
+    (100) NOT NULL,
+    role        varchar
+    (100) NOT NULL,
+    username    varchar
+    (100) NOT NULL,
+    password    varchar
+    (100) NOT NULL,
     hoursWorked float
 );
 
 
-create table restaurant_statistics
-(
-    creditRevenue FLOAT,
-    cashRevenue FLOAT,
-    tips FLOAT,
-    bestSeller varchar(100),
-    worstSeller varchar(100),
-    tablesServed varchar(100)
-);
+    create table restaurant_statistics
+    (
+        creditRevenue FLOAT,
+        cashRevenue FLOAT,
+        tips FLOAT,
+        bestSeller varchar(100),
+        worstSeller varchar(100),
+        tablesServed varchar(100)
+    );
+
+    -- here we insert values for each employee
+    -- managers
+    insert into jrestaurant_employees
+        (name, role, username, password, hoursWorked)
+    values
+        ('Jeff', 'manager', 'm1user', 'm1password', 40);
+
+    -- waiters
+    insert into jrestaurant_employees
+        (name, role, username, password, hoursWorked)
+    values
+        ('Sarah', 'waiter', 'w1user', 'w1password', 8.25),
+        ('Josh', 'waiter', 'w2user', 'w2password', 8),
+        ('Mike', 'waiter', 'w3user', 'w3password', 11);
+
+    -- busboys
+    insert into jrestaurant_employees
+        (name, role, username, password, hoursWorked)
+    values
+        ('Cameron', 'busboy', 'b1user', 'b1password', 6.5);
+
+    -- cooks
+    insert into jrestaurant_employees
+        (name, role, username, password, hoursWorked)
+    values
+        ('Lily', 'cook', 'cuser1', 'c1password', 7),
+        ('Charlotte', 'cook', 'c2user', 'c2password', 10);
+
 
 -- here we insert values for each employee
 -- managers
@@ -49,3 +83,12 @@ values ('Lily', 'cook', 'cuser1', 'c1password', 7),
 insert into jrestaurant_employees(name, role, username, password, hoursWorked)
 values ('Dean', 'host', 'huser1', 'h1password', 4);
 values ('Dean', 'host', 'huser1', 'h1password', 4);
+
+    -- hosts
+    insert into jrestaurant_employees
+        (name, role, username, password, hoursWorked)
+    values
+        ('Dean', 'host', 'huser1', 'h1password', 4);
+    values
+        ('Dean', 'host', 'huser1', 'h1password', 4);
+
