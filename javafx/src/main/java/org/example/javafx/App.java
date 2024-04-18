6c6aaa2ee2a7ee3e6af251e30dc0dfbf93f0f375
ryan <concepting@protonmail.com>
2024-04-18 16:46:59 -0400

    read me!!!
    gui's are almost completely done we just need to work on the logic section and adding a DB

--- javafx/src/main/java/org/example/javafx/App.java
+++ javafx/src/main/java/org/example/javafx/App.java
@@ -14,6 +14,18 @@ public class App extends Application {
       Scene scene = new Scene(fxmlLoader.load());
       stage.setScene(scene);
       stage.show();
+
+
+    //This is a test to see if the GUIs look proper
+//        FXMLLoader fxmlLoader2 = new FXMLLoader(App.class.getResource("/Fxml/Client/WaiterTable.fxml"));
+//        Scene scene2 = new Scene(fxmlLoader2.load());
+//        stage.setScene(scene2);
+//        stage.show();
+
+//        FXMLLoader fxmlLoader3 = new FXMLLoader(App.class.getResource("/Fxml/Model/DrinksMenu.fxml"));
+//        Scene scene3 = new Scene(fxmlLoader3.load());
+//        stage.setScene(scene3);
+//        stage.show();
     }
 }
 
