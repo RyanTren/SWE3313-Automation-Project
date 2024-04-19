328b42be2ab470a9bddb5c6b1ec427909fa83135
ryan <concepting@protonmail.com>
2024-04-19 15:10:09 -0400

    Init Receipt Controller and Fxml

--- javafx/src/main/java/org/example/javafx/Models/OrderReceiptController.java
+++ javafx/src/main/java/org/example/javafx/Models/OrderReceiptController.java
@@ -5,7 +5,7 @@ import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class EntreeMenuController implements Initializable {
+public class OrderReceiptController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
