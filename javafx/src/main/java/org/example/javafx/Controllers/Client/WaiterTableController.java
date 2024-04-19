821b70ad930ac3250f2dae4ddb05662fbeee7f0c
ryan <concepting@protonmail.com>
2024-04-19 13:39:12 -0400

    small fix

--- javafx/src/main/java/org/example/javafx/Controllers/Client/WaiterTableController.java
+++ javafx/src/main/java/org/example/javafx/Controllers/Client/WaiterTableController.java
@@ -1,155 +1,19 @@
-//package org.example.javafx.Controllers.Client;
-//
-//import javafx.fxml.FXML;
-//import javafx.fxml.FXMLLoader;
-//import javafx.fxml.Initializable;
-//import javafx.scene.control.Button;
-//import javafx.scene.control.ToggleButton;
-//import javafx.stage.Stage;
-//import org.example.javafx.Models.Model;
-//
-//import java.io.IOException;
-//import javafx.scene.control.ToggleButton;
-//
-//import java.net.URL;
-//import java.util.ResourceBundle;
-//
-//
-//public class WaiterTableController implements Initializable {
-//
-//
-//    // Add @FXML declarations for all 30 ToggleButtons
-//    @FXML private ToggleButton tableToggleButton1;
-//    @FXML private ToggleButton tableToggleButton2;
-//    @FXML private ToggleButton tableToggleButton3;
-//    @FXML private ToggleButton tableToggleButton4;
-//    @FXML private ToggleButton tableToggleButton5;
-//    @FXML private ToggleButton tableToggleButton6;
-//    @FXML private ToggleButton tableToggleButton7;
-//    @FXML private ToggleButton tableToggleButton8;
-//    @FXML private ToggleButton tableToggleButton9;
-//    @FXML private ToggleButton tableToggleButton10;
-//    @FXML private ToggleButton tableToggleButton11;
-//    @FXML private ToggleButton tableToggleButton12;
-//    @FXML private ToggleButton tableToggleButton13;
-//    @FXML private ToggleButton tableToggleButton14;
-//    @FXML private ToggleButton tableToggleButton15;
-//    @FXML private ToggleButton tableToggleButton16;
-//    @FXML private ToggleButton tableToggleButton17;
-//    @FXML private ToggleButton tableToggleButton18;
-//    @FXML private ToggleButton tableToggleButton19;
-//    @FXML private ToggleButton tableToggleButton20;
-//    @FXML private ToggleButton tableToggleButton21;
-//    @FXML private ToggleButton tableToggleButton22;
-//    @FXML private ToggleButton tableToggleButton23;
-//    @FXML private ToggleButton tableToggleButton24;
-//    @FXML private ToggleButton tableToggleButton25;
-//    @FXML private ToggleButton tableToggleButton26;
-//    @FXML private ToggleButton tableToggleButton27;
-//    @FXML private ToggleButton tableToggleButton28;
-//    @FXML private ToggleButton tableToggleButton29;
-//    @FXML private ToggleButton tableToggleButton30;
-//    @FXML private Button logoutButton;
-//
-//    @FXML
-//    public void initialize(URL url, ResourceBundle resourceBundle) {
-//        // Array to store all ToggleButtons
-//        ToggleButton[] tableToggleButtons = {
-//                tableToggleButton1,
-//                tableToggleButton2,
-//                tableToggleButton3,
-//                tableToggleButton4,
-//                tableToggleButton5,
-//                tableToggleButton6,
-//                tableToggleButton7,
-//                tableToggleButton8,
-//                tableToggleButton9,
-//                tableToggleButton10,
-//                tableToggleButton11,
-//                tableToggleButton12,
-//                tableToggleButton13,
-//                tableToggleButton14,
-//                tableToggleButton15,
-//                tableToggleButton16,
-//                tableToggleButton17,
-//                tableToggleButton18,
-//                tableToggleButton19,
-//                tableToggleButton20,
-//                tableToggleButton21,
-//                tableToggleButton22,
-//                tableToggleButton23,
-//                tableToggleButton24,
-//                tableToggleButton25,
-//                tableToggleButton26,
-//                tableToggleButton27,
-//                tableToggleButton28,
-//                tableToggleButton29,
-//                tableToggleButton30
-//        };
-//
-//        //Table Buttons
-//
-//
-//
-//        //loop is broken/doesn't work ... need to debug - ryan
-//
-//        // Loop through all ToggleButtons and add listeners
-//        for (int i = 1; i <= 30; i++) {
-//            ToggleButton button = getToggleButtonById("tableToggleButton" + i);
-//            if (button != null) {
-//                button.selectedProperty().addListener((observable, oldValue, newValue) -> {
-//                    if (newValue) {
-//                        String tableName = button.getText();
-//                        System.out.println("Selected Table: " + tableName);
-//                    }
-//                });
-//            }
-//        }
-//
-//        //Logout Button Logic
-//        logoutButton.setOnAction(event -> {
-//            performLogout();
-//        });
-//    }
-//
-//    // Helper method to get ToggleButton by ID dynamically
-//
-//
-//    private ToggleButton getToggleButtonById(String id) {
-//        ToggleButton button = null;
-//        try {
-//            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/Client/WaiterTable.fxml"));
-//            loader.load();
-//            button = (ToggleButton) loader.getNamespace().get(id);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        return button;
-//    }
-//
-//    // You can add more methods to handle other UI interactions or events
-//    private void performLogout() {
-//        // Perform logout actions here
-//        System.out.println("User logged out.");
-//
-//        // You may navigate to the login screen
-//        Stage stage = (Stage) logoutButton.getScene().getWindow();
-//        Model.getInstance().getViewFactory().closeStage(stage);
-//    }
-//}
-
 package org.example.javafx.Controllers.Client;
 
+import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import org.example.javafx.Models.Model;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class WaiterTableController implements Initializable {
@@ -186,7 +50,6 @@ public class WaiterTableController implements Initializable {
     @FXML private ToggleButton tableToggleButton29;
     @FXML private ToggleButton tableToggleButton30;
     @FXML private Button logoutButton;
-
     @FXML
     public void initialize(URL url, ResourceBundle resourceBundle) {
         // Loop through all ToggleButtons and add listeners
@@ -197,6 +60,7 @@ public class WaiterTableController implements Initializable {
                 button.selectedProperty().addListener((observable, oldValue, newValue) -> {
                     System.out.println("Button " + finalI + " clicked."); // Print a debug statement
                     if (newValue) {
+                        onClickTable(); // Call onClickTable method when a table button is selected
                         String tableName = "Table " + finalI; // Use finalI here instead of i
                         System.out.println("Selected Table: " + tableName);
                     }
@@ -208,73 +72,41 @@ public class WaiterTableController implements Initializable {
         logoutButton.setOnAction(event -> performLogout());
     }
 
-
     // Helper method to get ToggleButton by ID dynamically
     private ToggleButton getToggleButtonById(String id) {
-        switch (id) {
-            case "tableToggleButton1":
-                return tableToggleButton1;
-            case "tableToggleButton2":
-                return tableToggleButton2;
-            case "tableToggleButton3":
-                return tableToggleButton3;
-            case "tableToggleButton4":
-                return tableToggleButton4;
-            case "tableToggleButton5":
-                return tableToggleButton5;
-            case "tableToggleButton6":
-                return tableToggleButton6;
-            case "tableToggleButton7":
-                return tableToggleButton7;
-            case "tableToggleButton8":
-                return tableToggleButton8;
-            case "tableToggleButton9":
-                return tableToggleButton9;
-            case "tableToggleButton10":
-                return tableToggleButton10;
-            case "tableToggleButton11":
-                return tableToggleButton11;
-            case "tableToggleButton12":
-                return tableToggleButton12;
-            case "tableToggleButton13":
-                return tableToggleButton13;
-            case "tableToggleButton14":
-                return tableToggleButton14;
-            case "tableToggleButton15":
-                return tableToggleButton15;
-            case "tableToggleButton16":
-                return tableToggleButton16;
-            case "tableToggleButton17":
-                return tableToggleButton17;
-            case "tableToggleButton18":
-                return tableToggleButton18;
-            case "tableToggleButton19":
-                return tableToggleButton19;
-            case "tableToggleButton20":
-                return tableToggleButton20;
-            case "tableToggleButton21":
-                return tableToggleButton21;
-            case "tableToggleButton22":
-                return tableToggleButton22;
-            case "tableToggleButton23":
-                return tableToggleButton23;
-            case "tableToggleButton24":
-                return tableToggleButton24;
-            case "tableToggleButton25":
-                return tableToggleButton25;
-            case "tableToggleButton26":
-                return tableToggleButton26;
-            case "tableToggleButton27":
-                return tableToggleButton27;
-            case "tableToggleButton28":
-                return tableToggleButton28;
-            case "tableToggleButton29":
-                return tableToggleButton29;
-            case "tableToggleButton30":
-                return tableToggleButton30;
-            default:
-                return null;
-        }
+        return switch (id) {
+            case "tableToggleButton1" -> tableToggleButton1;
+            case "tableToggleButton2" -> tableToggleButton2;
+            case "tableToggleButton3" -> tableToggleButton3;
+            case "tableToggleButton4" -> tableToggleButton4;
+            case "tableToggleButton5" -> tableToggleButton5;
+            case "tableToggleButton6" -> tableToggleButton6;
+            case "tableToggleButton7" -> tableToggleButton7;
+            case "tableToggleButton8" -> tableToggleButton8;
+            case "tableToggleButton9" -> tableToggleButton9;
+            case "tableToggleButton10" -> tableToggleButton10;
+            case "tableToggleButton11" -> tableToggleButton11;
+            case "tableToggleButton12" -> tableToggleButton12;
+            case "tableToggleButton13" -> tableToggleButton13;
+            case "tableToggleButton14" -> tableToggleButton14;
+            case "tableToggleButton15" -> tableToggleButton15;
+            case "tableToggleButton16" -> tableToggleButton16;
+            case "tableToggleButton17" -> tableToggleButton17;
+            case "tableToggleButton18" -> tableToggleButton18;
+            case "tableToggleButton19" -> tableToggleButton19;
+            case "tableToggleButton20" -> tableToggleButton20;
+            case "tableToggleButton21" -> tableToggleButton21;
+            case "tableToggleButton22" -> tableToggleButton22;
+            case "tableToggleButton23" -> tableToggleButton23;
+            case "tableToggleButton24" -> tableToggleButton24;
+            case "tableToggleButton25" -> tableToggleButton25;
+            case "tableToggleButton26" -> tableToggleButton26;
+            case "tableToggleButton27" -> tableToggleButton27;
+            case "tableToggleButton28" -> tableToggleButton28;
+            case "tableToggleButton29" -> tableToggleButton29;
+            case "tableToggleButton30" -> tableToggleButton30;
+            default -> null;
+        };
     }
 
 
@@ -288,9 +120,33 @@ public class WaiterTableController implements Initializable {
         Model.getInstance().getViewFactory().closeStage(stage);
     }
 
-    private void onClick(){
-        Stage stage = (Stage) tableToggleButton1.getScene().getWindow();
-        Model.getInstance().getViewFactory().closeStage(stage);
-        Model.getInstance().getViewFactory().showDrinksMenu();
+//    private void onClick(){
+//        Stage stage = (Stage) tableToggleButton1.getScene().getWindow();
+//        Model.getInstance().getViewFactory().closeStage(stage);
+//        Model.getInstance().getViewFactory().showDrinksMenu();
+//    }
+
+
+    //tableButton now transitions to the DrinksMenu.fxml but, you have to double-click the button. (need to fix this)
+    private void onClickTable() {
+        try {
+            // Load the FXML file for the drinks menu
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/Model/DrinksMenu.fxml"));
+            AnchorPane drinksMenuPane = loader.load();
+
+            // Get the current stage
+            Stage currentStage = (Stage) tableToggleButton1.getScene().getWindow();
+
+            // Create a new stage for the drinks menu
+            Stage drinksMenuStage = new Stage();
+            drinksMenuStage.setScene(new Scene(drinksMenuPane));
+
+            // Close the current stage and show the drinks menu stage
+            currentStage.close();
+            drinksMenuStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 }
--- javafx/src/main/java/org/example/javafx/Models/DrinksMenuController.java
+++ javafx/src/main/java/org/example/javafx/Models/DrinksMenuController.java
@@ -14,8 +14,44 @@ import java.util.ResourceBundle;
 
 public class DrinksMenuController implements Initializable {
 
+    @FXML private Button logoutButton;
+
+    @FXML private Button backButton;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Logout Button Logic
+        logoutButton.setOnAction(event -> {
+            System.out.println("Logging out!");
+            performLogout();
+        });
 
+        // Back Button Logic
+        backButton.setOnAction(event -> {
+            System.out.println("User went back a screen.");
+            performBack();
+        });
     }
+
+    // Method to handle logout action
+    private void performLogout() {
+        // Perform logout actions here
+        System.out.println("User logged out.");
+
+        // You may navigate to the login screen
+        Stage stage = (Stage) logoutButton.getScene().getWindow();
+        Model.getInstance().getViewFactory().closeStage(stage);
+    }
+
+    // Method to handle back action
+    private void performBack() {
+        // Perform back actions here
+        System.out.println("User went back a screen.");
+
+        // You may navigate to the previous screen
+        Stage stage = (Stage) backButton.getScene().getWindow();
+        Model.getInstance().getViewFactory().closeStage(stage);
+        Model.getInstance().getViewFactory().showWaiterTableWindow();
+    }
+
 }
