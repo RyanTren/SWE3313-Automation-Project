583b88f0ebd68b97e2f9bf6ccd1e7884462f12f5
ryan <concepting@protonmail.com>
2024-04-24 15:49:38 -0400

    Update ManagerCreateAccountInfoController.java

--- javafx/src/main/java/org/example/javafx/Models/ManagerModel/ManagerCreateAccountInfoController.java
+++ javafx/src/main/java/org/example/javafx/Models/ManagerModel/ManagerCreateAccountInfoController.java
@@ -5,7 +5,7 @@ import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class ManagerAccountInfoController implements Initializable {
+public class ManagerCreateAccountInfoController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle){
 
